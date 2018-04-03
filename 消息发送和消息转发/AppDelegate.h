e43ee87d79a9397e70fc6ea9fd76e32ff686ccbc
james <layer_james@163.com>
2018-04-03 09:24:25 +0800

    Initial Commit

---
+++ 消息发送和消息转发/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  消息发送和消息转发
+//
+//  Created by james on 2018/4/3.
+//  Copyright © 2018年 james. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 消息发送和消息转发/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  消息发送和消息转发
+//
+//  Created by james on 2018/4/3.
+//  Copyright © 2018年 james. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
